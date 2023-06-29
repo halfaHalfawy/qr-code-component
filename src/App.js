5457f9151d2a84b00d3a77d97a2beaeee74cd5be
Mohammed abdalmonem <mabdalmonim474@gmail.com>
2023-06-29 15:52:52 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./image-qr-code.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+      <div className="card">
+        <div  className="ElementsColumn">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h4>
+          Improve your front-end skills by building projects
+          </h4>
+          <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
